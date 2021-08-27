b3a558b9848634ed0c9525cdf5845caa5ef35461
Amdremzzr <andremezzaliraribeiro@gmail.com>
2021-08-27 18:48:29 -0300

    colors adding into pokemons div

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -39,7 +39,7 @@ router.get('/', isAuthorized , async (req,res) => {
 
 router.get('/profile', isAuthorized , async (req,res) => {
     try{
-        res.send(200);
+        res.json(req.user);
     }
     catch(err) {
         console.log(err);
